09e5ae774b810595c2bfead0afaeeec29b7088c6
julianlinux <julianlinux@julianlinux-X555LA>
2016-03-04 12:32:17 +0100

    Terminado método iniciales

--- src/proyecto/Datos.java
+++ src/proyecto/Datos.java
@@ -3,6 +3,8 @@ package proyecto;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import javax.swing.JOptionPane;
 
@@ -15,26 +17,18 @@ import javax.swing.JOptionPane;
 public class Datos {
     
     
-    
-//    public void iniciales(){
-//      Scanner sc = new Scanner(System.in);
-//      String s;
-//        System.out.println("Intoduce tus iniciales");
-//        s= sc.nextLine();
-//    }
-    
  
          public void escribir_iniciales(String nomeFich){
-        File f = new File(nomeFich);
+        FileWriter f = null;
         PrintWriter escribir= null;
         try {
-           
+          f= new FileWriter(nomeFich,true); 
          escribir= new PrintWriter(f);
             String esc=JOptionPane.showInputDialog("Introduce tus inicinales");
             escribir.append(esc);
             escribir.append("\n");
                       
-        } catch (FileNotFoundException ex) {
+        } catch (IOException ex) {
             System.out.println("erro escritura"+ ex.getMessage());
            }
         finally{
