370d9f05d903bf0de3081d402f82a182aeebe2bf
Julián <Julin@192.168.1.33>
2016-03-05 03:00:09 +0100

    Clase Cronometro

---
+++ src/proyecto/Cronometro.java
@@ -0,0 +1,33 @@
+
+package proyecto;
+
+/**
+ *
+ * @author Julián
+ */
+public class Cronometro {
+    private long inicio, fim;
+ 
+    public void iniciar(){
+        inicio = System.currentTimeMillis();
+    }
+ 
+    public void parar(){
+        fim = System.currentTimeMillis();
+    }
+ 
+    public long getTempo() {
+        return fim-inicio;
+        
+    }
+
+ 
+    public double getSegundos() {
+        return (fim - inicio) / 1000.0;
+    }
+ 
+    public double getMinutos() {
+        return (fim - inicio) / 60000.0;
+    }
+    
+}
--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -1,4 +1,3 @@
-
 package proyecto;
 
 /**
@@ -7,12 +6,16 @@ package proyecto;
  */
 public class Proyecto {
 
-    
     public static void main(String[] args) {
-        Datos obj1= new Datos();
-        
+
+        Datos obj1 = new Datos();
+        Cronometro t = new Cronometro();
+        t.iniciar();
+
         obj1.escribir_iniciales("Iniciales.txt");
-        
+
+        t.parar();
+        System.out.println(t.getTempo());
+
     }
-    
 }
