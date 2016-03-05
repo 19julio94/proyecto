ea5cfac7b49b854d417f0f992cb32370c9d81702
Julián <Julin@192.168.1.33>
2016-03-06 00:57:54 +0100

    Change

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -1,5 +1,7 @@
 package proyecto;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Julián
@@ -11,11 +13,17 @@ public class Proyecto {
         Datos obj1 = new Datos();
         Cronometro t = new Cronometro();
         t.iniciar();
-
+//**************************************
         obj1.escribir_iniciales("Iniciales.txt");
 
+        
+        
+        
+        
+        
+  //*****************************************************      
         t.parar();
-        System.out.println(t.getTempo());
+        System.out.println("Minutos "+t.getMinutos()+"\n"+"segundos "+t.getSegundos());
 
     }
 }
